6cb4df428d1f8f46b214ea3d43022a4c7e300fea
farizaiis <farizaiis@hotmail.com>
2021-12-01 13:49:15 +0700

    answer for test4

--- test4.js
+++ test4.js
@@ -5,10 +5,19 @@
  * Expected Result:
  * 8
  */
-const numbers = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+const numbers = [14, 9, 6, 4, 2, 3, 5, 7, 0];
 
 function result(numbers) {
-  // Your Code Here
+    const minNumbers = Math.min(...numbers);
+    const maxNumbers = Math.max(...numbers);
+    const missingNumbers = [];
+
+    for (let i = minNumbers; i <= maxNumbers; i++) {
+        if (!numbers.includes(i)) {
+            missingNumbers.push(i);
+        }
+    }
+    return missingNumbers;
 }
 
 console.log(result(numbers));
