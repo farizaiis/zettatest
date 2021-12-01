aa5ceba17241a4a5ceaa51cf0bc7308a2669f780
farizaiis <farizaiis@hotmail.com>
2021-12-01 13:23:39 +0700

    add answer for test2

--- test2.js
+++ test2.js
@@ -1,14 +1,26 @@
 /**
  * Direction:
  * Remove duplicated data from array
- * 
+ *
  * Expected Result:
  * [1, 2, 3, 4, 5]
  */
 const data = [1, 4, 2, 3, 5, 3, 2, 4];
 
 function result(data) {
-  // Your Code Here
+    let newData = [];
+    let newDataIndex = 0;
+    let temp = {};
+
+    for (let i = 0; i < data.length; i++) {
+        if (!temp[data[i]]) {
+            temp[data[i]] = 1;
+            newData[newDataIndex] = data[i];
+            newDataIndex++;
+        }
+    }
+
+    return newData
 }
 
 console.log(result(data));
