87f5a7b341fc4c89bc7ba8edc9c80b62aed549bf
farizaiis <farizaiis@hotmail.com>
2021-12-01 14:00:51 +0700

    add answer for test5

--- test5.js
+++ test5.js
@@ -8,7 +8,21 @@
 const words = ['flower', 'flow', 'flight'];
 
 function result(words) {
-  // Your Code Here
+    if (!words || words.length === 0) {
+        return '';
+    }
+
+    const sortWordsByLength = words.sort((a, b) => {
+        a.length - b.length;
+    });
+
+    let prefix = sortWordsByLength[0];
+
+    while (!words.every((string) => string.startsWith(prefix))) {
+        prefix = prefix.slice(0, -1);
+    }
+
+    return prefix;
 }
 
 console.log(result(words));
